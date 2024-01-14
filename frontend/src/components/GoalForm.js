7e3caaf39ffbf93913382420ea5f9932b52da45c
Amaan Farooqi <amaan.farooqi2020@gmail.com>
2024-01-14 01:23:42 -0500

    Fixed form tag and submit button issues preventing submission of goals

--- frontend/src/components/GoalForm.js
+++ frontend/src/components/GoalForm.js
@@ -1,35 +1,38 @@
-import { useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import {createGoal} from "../features/goals/goalSlice"
-
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { createGoal } from "../features/goals/goalSlice";
 
 function GoalForm() {
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
 
-    const dispatch = useDispatch()
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createGoal({ text }));
+    setText('');
+  };
 
-    const [text, setText] = useState('')
-  
-    const onSubmit = e => {
-    e.preventDefault()
-
-    dispatch(createGoal({text}))
-    setText('')
-
-}  
-  return <section className='form'>
-        <form onSubmit={onSubmit}></form>
+  return (
+    <section className="form">
+      <form onSubmit={onSubmit}>
         <div className="form-group">
-            <label htmlFor="text">Goal</label>
-            <input type="text" name="text" id="text" 
-            value = {text} onChange={(e) => setText(e.target.value)}/>
+          <label htmlFor="text">Goal</label>
+          <input
+            type="text"
+            name="text"
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
         </div>
         <div className="form-group">
-            <button className="btn btn-block" type="submit">
-                Add Goal
-            </button>
+          <button className="btn btn-block" type="submit">
+            Add Goal
+          </button>
         </div>
+      </form>
     </section>
-  
+  );
 }
 
-export default GoalForm
+export default GoalForm;
